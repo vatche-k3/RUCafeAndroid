77cb0314cdbea2713dc9fbbc78efbd53fa7f4a9e
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-04-15 21:02:20 -0400

    :building_construction: Completely redid entire view
    We have to use activities, not fragments in order to get credit.

---
+++ app/src/main/java/com/rucafe/ui/OrderingDonutActivity.java
@@ -0,0 +1,16 @@
+package com.rucafe.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rucafe.R;
+
+public class OrderingDonutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordering_donut);
+    }
+}
---
+++ app/src/main/java/com/rucafe/ui/OrdersPageActivity.java
@@ -0,0 +1,16 @@
+package com.rucafe.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rucafe.R;
+
+public class OrdersPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orders_page);
+    }
+}
